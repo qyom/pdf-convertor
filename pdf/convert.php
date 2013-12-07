e6e8f891131a548559dce939a8659a8a5f7dbba8
agalyan <agalyan@ymail.com>
2013-12-07 13:36:13 +0400

    ticket #55

--- pdf/convert.php
+++ pdf/convert.php
@@ -1,15 +1,6 @@
-<?php header('Access-Control-Allow-Origin: *');
+<?php // header('Access-Control-Allow-Origin: *');
 
 require_once('WkHtmlToPdf.php');
-
-$pdf = new WkHtmlToPdf;
-
-$pdf->setPageOptions(array(
-    'orientation' => 'landscape',
-//    'page-size' => 'A4',
-    'page-height' => '390mm',
-    'page-width' => '255mm',
-));
 
 $pdfData = '';
 if(isset($_POST['pdfData'])){
@@ -20,6 +11,20 @@ $saveToFile = false;
 if(isset($_POST['saveToFile'])){
     $saveToFile = true;
 };
+
+$orientation = 'landscape';
+if(isset($_POST['portrait'])){
+    $orientation = 'portrait';
+};
+
+$pdf = new WkHtmlToPdf;
+
+$pdf->setPageOptions(array(
+    'orientation' => $orientation,
+//    'page-size' => 'A4',
+    'page-height' => '400mm',
+    'page-width' => '270mm',
+));
 
 // Add a HTML file, a HTML string or a page from URL
 //  $pdf->addPage('http://google.com');
@@ -45,12 +50,25 @@ $pdf->addPage('/home/zizicoco/www/utils/pdf/tmp/tmp.html');
 if($saveToFile){
 
     if(!$pdf->send("linesheet.pdf")){
-        throw new Exception('Could not save PDF: '.$pdf->getError());
+        print "Something went wrong on downloading the file, please try again.<br/>";
+        print "If problem persists please contact our customer support.";
+        // throw new Exception('Could not save PDF: '.$pdf->getError());
+        // trigger_error("Incorrect input vector, array of values expected", E_USER_WARNING);
     }
 
 }
 else{
     if(!$pdf->send()){
-        throw new Exception('Could not display PDF: '.$pdf->getError());
+
+        print "Something went wrong on displaying the preview, please try again.<br/>";
+        print "If problem persists please contact our customer support.";
+        // throw new Exception('Could not display PDF: '.$pdf->getError());
+        // trigger_error("Incorrect input vector, array of values expected", E_USER_WARNING);
     }
 }
+
+function errorReporter() {
+    echo "Error message goes here";
+}
+
+set_error_handler('errorReporter');
